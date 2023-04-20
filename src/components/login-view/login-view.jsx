6a54f3d64ef7d2f5c0ce077d0cef2e1c4808feb5
OBo1307 <och.bogdan@gmail.com>
2023-04-20 20:18:27 +0100

    added SignUp and Login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password
+    };
+
+    fetch("https://boflixapp.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert("We don't have this user");
+      }
+    })
+    .catch((e) => {
+      alert("Something went wrong");
+    });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -23,3 +60,4 @@
     </form>
   );
 };
+
